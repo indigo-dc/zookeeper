573772511fb911f6e81071307069167b55a80135
Marica Antonacci <marica.antonacci@ba.infn.it>
2016-07-20 14:36:45 +0200

    Fixing duplicated entries in the conf on restart (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,19 @@
 
 echo ${MYID:-1} > /etc/zookeeper/conf/myid
 
+ZOO_CONF_FILE=/etc/zookeeper/conf/zoo.cfg
+
 if [ ! -z "$SERVERS" ]; then
+ # clean the conf - needed in case of restart
+ sed -i '/^server\.[0-9]\+=.*/d' $ZOO_CONF_FILE
+
  IFS=',' read -r -a array <<< "$SERVERS"
  j=1;
  for server in ${array[@]}; do 
-    echo server.$j=$server:2888:3888  >> /etc/zookeeper/conf/zoo.cfg
+    echo server.$j=$server:2888:3888  >> $ZOO_CONF_FILE
     ((j=j+1))
  done 
+
 fi
 
 exec "$@"
